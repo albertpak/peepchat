4672eb635f4340c0390fef7c7e2515550507485a
Albert Pak <al@albert-pak.com>
2017-06-02 18:10:58 -0500

    first Elixir API

---
+++ api/web/controllers/session_controller.ex
@@ -0,0 +1,11 @@
+defmodule Peepchat.SessionController do
+  use Peepchat.Web, :controller
+
+  def index(conn, _params) do
+    # Return some static JSON for now
+
+    conn
+
+    |> json(%{status: "Ok"})
+  end
+end

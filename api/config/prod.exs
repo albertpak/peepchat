0ad56fc6b04418ab05624338697c7e112328dff3
Albert Pak <al@albert-pak.com>
2017-06-03 00:39:17 -0500

    database setup

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -14,7 +14,13 @@ use Mix.Config
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
+
+config :peepchat, Peepchat.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
